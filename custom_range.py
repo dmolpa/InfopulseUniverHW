a8edb118671afc2cad71ea25b2ec2d0eafbed212
Dmytro <37658654+dmolpa@users.noreply.github.com>
2018-04-26 23:18:53 +0300

    Add files via upload

--- custom_range.py
+++ custom_range.py
@@ -1,7 +1,7 @@
-def custom_range(start,stop):
+def custom_range(stop=0,start=0):
     yield start
     if start+1 < stop:
-        yield from custom_range(start+1,stop)
+        yield from custom_range(stop,start+1)
 
-for i in custom_range(0,14):
+for i in custom_range(14):
     print(i)
